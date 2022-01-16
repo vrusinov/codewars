6eb1b38c49318057161fd9fd5ddd1b9caa0050aa
Vladimir Rusinov <vladimir.rusinov@gmail.com>
2022-01-16 18:29:38 +0000

    Implement square root kata - in Rust

--- 6_kyu/sum_of_digits/src/main.rs
+++ 6_kyu/sum_of_digits/src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+fn sum_of_digits(n: i64) -> i64 {
+    let mut result: i64 = 0;
+    let mut reminder: i64 = n;
+    while reminder >= 10 {
+        result += reminder % 10;
+        reminder = reminder / 10;
+    }
+    result += reminder;
+    return result;
+}
+
+fn digital_root(n: i64) -> i64 {
+    println!("{}", n);
+    let mut result: i64 = n;
+    while result >= 10 {
+        result = sum_of_digits(result);
+    }
+    return result;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn returns_expected() {
+      assert_eq!(digital_root(16), 7);
+      assert_eq!(digital_root(942), 6);
+      assert_eq!(digital_root(132189), 6);
+      assert_eq!(digital_root(493193), 2);
+
+      assert_eq!(digital_root(1), 1);
+      assert_eq!(digital_root(9), 9);
+      assert_eq!(digital_root(0), 0);
+      assert_eq!(digital_root(11), 2);
+      assert_eq!(digital_root(29), 2);
+      assert_eq!(digital_root(10), 1);
+    }
 }
