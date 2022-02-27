527b4b5a9364cb613942edac8fccc577c1aedf2a
Vladimir Rusinov <vladimir.rusinov@gmail.com>
2022-02-27 22:06:37 +0000

    Sort Numbers

---
+++ 7_kyu/sort_numbers/README.md
@@ -0,0 +1,5 @@
+# Sort Numbers
+
+https://www.codewars.com/kata/5174a4c0f2769dd8b1000003
+
+Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
---
+++ 7_kyu/sort_numbers/src/main.rs
@@ -0,0 +1,22 @@
+fn sort_numbers(arr: &Vec<i32>) -> Vec<i32> {
+    // Must copy the vector.
+    // to_vec Copies self into a new Vec
+    // https://doc.rust-lang.org/std/primitive.slice.html#method.to_vec
+    let mut newvec = arr.to_vec();
+    newvec.sort();
+    return newvec;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn sample_tests() {
+        assert_eq!(sort_numbers(&vec![1, 2, 3, 10, 5]), vec![1, 2, 3, 5, 10]);
+        assert_eq!(sort_numbers(&vec![]), vec![]);
+        assert_eq!(sort_numbers(&vec![20, 2, 10]), vec![2, 10, 20]);
+        assert_eq!(sort_numbers(&vec![2, 20, 10]), vec![2, 10, 20]);
+        assert_eq!(sort_numbers(&vec![2, 10, 20]), vec![2, 10, 20]);
+    }
+}
