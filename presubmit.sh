485c95290cd35f89b0ed12bc279ad9e45261e250
Vladimir Rusinov <vladimir.rusinov@gmail.com>
2022-05-03 21:58:21 +0100

    Make some checks optional in presubmit

--- presubmit.sh
+++ presubmit.sh
@@ -5,7 +5,7 @@ set -e
 # Run gometalinter in all go directories
 for d in $(find . -name \*.go | sed -r 's|/[^/]+$||' |sort -u) ; do
     golint $d
-    gometalinter $d
+    which gometalinter && gometalinter $d
     pushd $d
     golangci-lint run
     popd
@@ -13,4 +13,4 @@ done
 
 pytype .
 
-docker run --rm -e "WORKSPACE=${PWD}" -v "$PWD:/app" shiftleft/sast-scan scan --build
+which docker && docker run --rm -e "WORKSPACE=${PWD}" -v "$PWD:/app" shiftleft/sast-scan scan --build
