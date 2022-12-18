d6c607ab773747d06c49bc3605497a8a5262f92a
Vladimir Rusinov <vladimir.rusinov@gmail.com>
2022-12-18 16:50:45 +0000

    Valid braces

---
+++ 6_kyu/valid_braces/valid_braces.py
@@ -0,0 +1,28 @@
+# SPDX-FileCopyrightText: 2022 Vladimir Rusinov <vladimir.rusinov@gmail.com>
+#
+# SPDX-License-Identifier: Apache-2.0
+"""Valid Braces
+
+https://www.codewars.com/kata/5277c8a221e209d3f6000b56/train/python
+"""
+
+BRACE_PAIRS = {
+    '[': ']',
+    '(': ')',
+    '{': '}',
+}
+
+
+def valid_braces(string):
+    braces = []
+    for brace in string:
+        # If it's an opening brace add to braces stack
+        if brace in BRACE_PAIRS:
+            braces.append(brace)
+        else:
+            # Check if it corresponds to the one on top of the braces stack
+            if braces and BRACE_PAIRS[braces.pop()] != brace:
+                return False
+    if braces:
+        return False
+    return True
---
+++ 6_kyu/valid_braces/valid_braces_test.py
@@ -0,0 +1,31 @@
+# SPDX-FileCopyrightText: 2022 Codewars
+#
+# SPDX-License-Identifier: Apache-2.0
+import codewars_test as test
+
+from valid_braces import valid_braces
+
+def assert_valid(string):
+    test.assert_equals(valid_braces(string), True, 'Expected "{0}" to be valid'.format(string))
+
+def assert_invalid(string):
+    test.assert_equals(valid_braces(string), False, 'Expected "{0}" to be invalid'.format(string))
+
+
+@test.describe("Valid Braces")
+def tests():
+	@test.it("sample Tests")
+	def sample_tests():
+		assert_valid( "()" )
+		assert_invalid( "(}" )
+		assert_valid( "[]" )
+		assert_invalid("[(])")
+		assert_valid( "{}" )
+		assert_valid( "{}()[]" )
+		assert_valid( "([{}])" )
+		assert_invalid( "([}{])" )
+		assert_valid( "{}({})[]" )
+		assert_valid( "(({{[[]]}}))" )
+		assert_invalid( "(((({{" )
+		assert_invalid( ")(}{][" )
+		assert_invalid( "())({}}{()][][" )
