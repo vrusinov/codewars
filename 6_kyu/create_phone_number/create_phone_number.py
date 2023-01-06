e0c38d196af9b81aa2ccde13123df6b76a9c2e25
Vladimir Rusinov <vladimir.rusinov@gmail.com>
2023-01-06 14:09:14 +0000

    Reformat

--- 6_kyu/create_phone_number/create_phone_number.py
+++ 6_kyu/create_phone_number/create_phone_number.py
@@ -7,7 +7,7 @@ from typing import Sequence
 
 def create_phone_number(n: Sequence[int]) -> str:
     str_n = [str(x) for x in n]
-    code = ''.join(str_n[:3])
-    part_1 = ''.join(str_n[3:6])
-    part_2 = ''.join(str_n[6:])
-    return f'({code}) {part_1}-{part_2}'
+    code = "".join(str_n[:3])
+    part_1 = "".join(str_n[3:6])
+    part_2 = "".join(str_n[6:])
+    return f"({code}) {part_1}-{part_2}"
