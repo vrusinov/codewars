6375314021749d0b236b0605457685eb0f364a11
Vladimir Rusinov <vladimir.rusinov@gmail.com>
2023-05-20 20:13:15 +0100

    Upgrade golangci-lint

--- presubmit.sh
+++ presubmit.sh
@@ -6,13 +6,6 @@
 
 set -e
 
-# Run gometalinter in all go directories
-for d in $(find . -name \*.go | sed -r 's|/[^/]+$||' |sort -u) ; do
-    pushd $d
-    golangci-lint run
-    popd
-done
-
 pytype .
 
 reuse lint
