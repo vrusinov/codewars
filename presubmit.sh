5850899c2d13a980e199307bf430dd05d1279706
Vladimir Rusinov <vladimir.rusinov@gmail.com>
2024-12-24 15:57:40 +0000

    Update go.mod

--- presubmit.sh
+++ presubmit.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-# SPDX-FileCopyrightText: 2022 Vladimir Rusinov
-#
+# SPDX-FileCopyrightText: 2022-2024 Vladimir Rusinov
 # SPDX-License-Identifier: Apache-2.0
 
 set -e
 
 pytype .
-
-reuse lint
-
-# Does not work very well:
-# which docker && docker run --rm -e "WORKSPACE=${PWD}" -v "$PWD:/app" shiftleft/sast-scan scan
