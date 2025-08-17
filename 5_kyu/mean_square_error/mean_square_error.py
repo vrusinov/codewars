50c05cfbfea9194aa9b62363a4823240b5f39f5c
Vladimir Rusinov <vladimir.rusinov@gmail.com>
2025-08-17 20:27:28 +0100

    Mean squared error

---
+++ 5_kyu/mean_square_error/mean_square_error.py
@@ -0,0 +1,16 @@
+# SPDX-FileCopyrightText: 2025 Vladimir Rusinov
+# SPDX-License-Identifier: Apache-2.0
+"""Mean Square Error
+
+https://www.codewars.com/kata/51edd51599a189fe7f000015/train/python
+"""
+
+def solution(array_a, array_b):
+    sum_of_squared_differences = 0
+    for a, b in zip(array_a, array_b):
+        difference = a - b
+        sum_of_squared_differences += difference ** 2
+
+    if len(array_a) > 0:
+        return sum_of_squared_differences / len(array_a)
+    return 0
