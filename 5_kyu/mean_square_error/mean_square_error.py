316b55b46f324d89ee88f6b370b4f224d3781316
Vladimir Rusinov <vladimir.rusinov@gmail.com>
2025-12-28 21:16:59 +0000

    Add python type annotations

--- 5_kyu/mean_square_error/mean_square_error.py
+++ 5_kyu/mean_square_error/mean_square_error.py
@@ -5,11 +5,15 @@
 https://www.codewars.com/kata/51edd51599a189fe7f000015/train/python
 """
 
-def solution(array_a, array_b):
+from typing import Sequence
+
+
+def solution(array_a: Sequence[int], array_b: Sequence[int]) -> float:
+    """Calculate the Mean Square Error between two arrays."""
     sum_of_squared_differences = 0
     for a, b in zip(array_a, array_b):
         difference = a - b
-        sum_of_squared_differences += difference ** 2
+        sum_of_squared_differences += difference**2
 
     if len(array_a) > 0:
         return sum_of_squared_differences / len(array_a)
